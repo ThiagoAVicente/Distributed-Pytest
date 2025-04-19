364e02e002a0660df150f42a337f335caad59512
vcnt <thiago.vicente@ua.pt>
2025-04-19 18:00:42 +0100

    updated node.py: added timeouts

--- src/app/node.py
+++ src/app/node.py
@@ -1,5 +1,6 @@
 import socket
 import os
+from typing import Optional, Dict
 
 from protocol import CDProto, CDProtoBadFormat
 
@@ -9,9 +10,10 @@ import time
 import logging
 
 DISCOVERY_PORT = 9876
-BROADCAST_INTERVAL = 5
-DISCOVERY_TIMEOUT = 10
-
+BROADCAST_INTERVAL = 5.
+DISCOVERY_TIMEOUT = 10.
+NOTIFY_INTERVAL = 15.
+CENTER_TIMEOUT = NOTIFY_INTERVAL * 1.5
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -19,32 +21,34 @@ class Node:
 
     def __init__(self):
 
+        # Node variables
         self.done = False
+        self.working = False
+        self.startElection:bool = False
+        self.connected = True if os.getenv("FIRST") == "True" else False
+        self.is_center = self.connected
+        self.lastCenterCheck:Optional[float] = None
+        self.lastNotify:float = -1
 
-        # get port that docker is using
-        self.port = os.getenv("PORT")
-        self.connected = False
-
-        ### TODO: remove and implement election
-        if int(self.port) == 8001:
-            self.connected = True
-        ####
-
+        # get port that docker is using env
         self.ip = socket.gethostbyname(socket.gethostname())
-        self.timeout:int = 3
+        self.port = os.getenv("PORT")
 
         # socket creation
+        self.timeout:int = 3
         self.sock:socket.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.settimeout(self.timeout)
 
+        # info about the network
+        self.center_node:Optional[tuple] = (self.ip,self.port) if self.is_center else None
         self.peers = []
 
+        # threading variables
         self.lock = threading.Lock()
 
     def __thread_safe_append(self,other):
         with self.lock:
             self.peers.extend(other)
-
 
     def __broadcast_discovery(self):
 
@@ -100,6 +104,18 @@ class Node:
             except Exception as e:
                 logging.error(f"Discovery listener error: {e}")
 
+    def __create_discovery_thread(self):
+        # create a thread for
+        discovery_thread = threading.Thread(target=self.__discovery_listen, daemon=True)
+        discovery_thread.start()
+        logging.info("✅ Discovery listener thread started after connection")
+
+    def __notify_peers(self):
+        mssg = {
+            "type":T.LIVECHECK,
+        }
+        for peer in self.peers:
+            CDProto.send(self.sock,peer,mssg)
 
     def run(self):
         self.sock.bind(('0.0.0.0', int(self.port)))
@@ -121,24 +137,45 @@ class Node:
 
                     # add peers
                     to_add = data["peers"]
-                    to_add.append(address)
+                    self.center_node = address
                     self.__thread_safe_append(to_add)
 
                     self.connected = True
-
+                    self.lastCenterCheck = time.time()
 
             if time.time() - start_time >= DISCOVERY_TIMEOUT and not self.connected:
                 self.connected = True
                 logging.info(
                     "🌟 No responses received within timeout. Starting a new network as the first node.")
 
-        discovery_thread = threading.Thread(target=self.__discovery_listen, daemon=True)
-        discovery_thread.start()
-        logging.info("✅ Discovery listener thread started after connection")
+        if self.is_center:
+            # only center node will inform new nodes about the network
+           self.__create_discovery_thread()
 
         while not self.done:
-            data = CDProto.recv(self.sock)
+            if time.time() - self.lastNotify  > NOTIFY_INTERVAL:
+                if self.is_center:
+                    # notify other nodes that center is ok
+                    self.__notify_peers()
+                else:
+                    # notify center node about node status
+                    ty:T = T.WORKING if self.working else T.FREE
+                    mssg = {
+                        "type" : ty
+                    }
+                    CDProto.send(self.sock,self.center_node,mssg)
+                self.lastNotify = time.time()
+
+            data:Optional[Dict] = CDProto.recv(self.sock)
             if data is not None:
-                # TODO
-                logging.info(threading.enumerate())
-                continue
+                cmd = data["type"]
+
+                if cmd == "LIVECHECK":
+                    self.lastCenterCheck = time.time()
+
+            if self.is_center: continue
+
+            # check if there is need for election
+            if time.time() - self.lastCenterCheck > CENTER_TIMEOUT:
+                logging.info(f"Didn't received livecheck from center. Starting election")
+                self.startElection = True
