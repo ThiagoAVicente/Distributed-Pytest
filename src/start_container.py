04c43c0e74a7ed883d8897b588d3b2d507c3a01e
vcnt <thiago.vicente@ua.pt>
2025-04-19 18:00:01 +0100

    updated start_container.py: now -s indicates if a new network must be started

--- src/start_container.py
+++ src/start_container.py
@@ -1,6 +1,9 @@
 import subprocess
 import os
 import argparse
+import time
+
+newNetwork = False
 
 def build_image() ->None :
     """
@@ -17,6 +20,8 @@ def start_containers(n:int) -> None:
     :param n: amount of containers to create
     :return:
     """
+    global newNetwork
+
     for index in range(n):
         container_name = f"container{index + 1}"
         port = f"800{index + 1}"
@@ -27,10 +32,12 @@ def start_containers(n:int) -> None:
             "-p", f"{port}:8000",
             "-v", f"{os.getcwd()}/app:/app",
             "-e", f"PORT={port}",
+            "-e",f"FIRST={newNetwork}",
             "python13-node"
         ], check=True)
-
+        newNetwork = False
         print(f"Container {container_name} started.")
+        time.sleep(0.5)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Build Docker image and start containers.")
@@ -42,7 +49,17 @@ if __name__ == "__main__":
         help="Number of containers to start (default: 3)"
     )
 
+    parser.add_argument(
+        '-s', '--start-network',
+        action='store_true',
+        help="Flag to start network containers (default: False)"
+    )
+
     args = parser.parse_args()
+
+    if args.start_network:
+        newNetwork = True
+        print("Starting a new network")
 
     # docker calls
     build_image()
